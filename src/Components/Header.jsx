43f2b94ea64530346f4a0fa0b1fb0ea2675dfc08
Dulanjna Rathnayaka <dulanjanarmd@gmail.com>
2026-08-06 03:09:56 +0530

    feat: create responsive Header component with navigation and social media links

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -26,6 +26,7 @@ const Header = () => {
           <li><a href="#home">Home</a></li>
           <li><a href="#portfolio">Projects</a></li>
           <li><a href="#about">About</a></li>
+          <li><a href="#education">Education</a></li>
           <li><a href="#footer">Contacts</a></li>
         </ul>
       </nav>
